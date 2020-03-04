ecf49046d533aec387dc08027b87d81d414c2e04
esfandiar.alireza <a@c.om>
2020-03-04 14:29:56 +0330

    add branch

--- apps/my-ngpro/src/app/app.module.ts
+++ apps/my-ngpro/src/app/app.module.ts
@@ -6,11 +6,11 @@ import { LoginComponent } from './src/auth/login/login.component';
 import { HomeComponent } from './src/homePages/home/home.component';
 import { AppRoutingModule } from './src/app-routing.module';
 import { RouterModule } from '@angular/router';
-import { CardModule } from 'primeng';
+import { ButtonModule, CardModule, InputTextModule } from 'primeng';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule, CardModule],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, CardModule, InputTextModule, ButtonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
